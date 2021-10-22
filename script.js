ad2332b0d0a54dcb57c57903142f51993c3978a8
RandoUji2223 <92761036+OfficerGregg@users.noreply.github.com>
2021-10-22 11:51:00 -1000

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function clickbutton(){
   //automatically take user to page two
   location.href="two.html";
 }
-function giveUser(){
+function getUser(){
   var b = localStorage.getItem(uName);
   document.getElementById('placeholder').innerHTML = "Hello " + b;
 }
